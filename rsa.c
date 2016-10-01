93cdb261902750f5c9a7ff8c97a9873157460092
LEXUGE <lexuge@qq.com>
2016-10-01 13:30:47 +0800

    Add files via upload

--- rsa.c
+++ rsa.c
@@ -4,7 +4,15 @@
 
 int start();
 int f(int m,int n);
+int prime(int n);
 
+int prime(int n)
+{
+  int i=0;
+  for (i=2;i*i<=n;i++)
+    if (n%i==0) return 0;
+  return 1;
+}
 int f(int m,int n)
 {
   int c=0;
@@ -21,21 +29,23 @@ int start()
   printf("RSA tool\n");
   printf("please input two prime number:");
   scanf("%d %d",&p,&q);
+  if ((prime(p)!=1)||(prime(q)!=1)) {printf("number(s) is(are) not the prime number!\n");exit(0);}
   n=p*q;
   l=(p-1)*(q-1)/f(p-1,q-1);
   srand((unsigned)time(NULL));
   while (1)
-  {
-    temp=rand()%(l-3);
+  {  
+    temp=(rand()%(l-4))+2;
     if (f(l,temp)==1) {e=temp;break;}
   }
-  srand((unsigned)time(NULL));
+  printf("(E,N)=(%d,%d)\n",e,n);
   while (1)
   {
-    temp=rand()%(l-3);
-    if ((temp*e%l==1)&&(temp!=e)) {d=temp;break;}
+    srand((unsigned)time(NULL));
+    temp=(rand()%(l-4))+2;
+    if (((temp*e)%l==1)&&(temp!=e)) {d=temp;break;}  
   }
-  printf("(E,N)=(%d,%d)\n",e,n);
+
   printf("(D,N)=(%d,%d)\n",d,n);
   return 0;
 }
